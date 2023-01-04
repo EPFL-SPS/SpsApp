315f74b15c11c65d76cf15be30ef0c0c0008a52b
Jonathan Michel <jonathanmichel@bluewin.ch>
2023-01-04 16:18:34 +0100

    Bugfix cards display

--- var.js
+++ var.js
@@ -1,9 +1,8 @@
 var cardTemplate = (title, description, more, imgSrc, animationDelay) => `
-<div class="col-sm-6 col-md-6 col-lg-4 col-xl-3 d-flex align-items-stretch animate slide" style=\"animation-delay: ${animationDelay}s;\">
-    <div class="card border-light mb-4 shadow">
-        <div class="card-header p-0">
-            <img class="bd-placeholder-img card-img-top img-fluid" width="100%" height="160" src="assets/thumbs/${imgSrc}"></img>
-
+<div class="col-12 col-sm-6 col-md-5 col-lg-4 col-xl-3 d-flex animate slide" style=\"animation-delay: ${animationDelay}s;\">
+    <div class="card border-light mb-4 shadow" style="width: 100%">
+        <div class="card-header p-0" style="height: 150px">
+            <img class="bd-placeholder-img card-img-top img-fluid" style="height: 100%; object-fit: cover" src="assets/thumbs/${imgSrc}"></img>
         </div>
 
         <div class="card-body">
@@ -13,7 +12,8 @@ var cardTemplate = (title, description, more, imgSrc, animationDelay) => `
          <div class="card-footer text-muted">
                 <div class="d-flex justify-content-between">
                     <div class="btn-group">
-                        <button type="button" class="btn btn-sm btn-dark">Plus d'informations</button>
+                    <!-- Add link that will open in a new tab -->
+                        <a href="https://sps.epfl.ch/" target="_blank"><button type="button" class="btn btn-sm btn-dark">Plus d'informations</button></a>
                     </div>
                     <small class="text-muted">${more}</small>
                 </div>  
